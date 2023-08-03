3a99f970ee1cda9b04c59c52073a617eb759e08c
Suryansh Singh <suryansh.singh@IM9764-X0.corp.inmobi.com>
2023-08-03 18:05:44 +0530

    kubernetes

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,9 @@ cd ..
 mvn clean install -DskipTests
 docker build --tag=ad_fraud_detection:v1 ./bin/
 cd bin
+
+docker tag bb6c5dc3fdba suryanshsingh/ad_fraud_detection:v1
+docker login --username suryanshsingh --password Devansh_123
+docker push suryanshsingh/ad_fraud_detection:v1
+
 docker-compose up --build
