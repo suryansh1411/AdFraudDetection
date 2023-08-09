ce0723a5e36cce7ebf8b309159935d44fc457e1b
Suryansh Singh <suryansh.singh@IM9764-X0.corp.inmobi.com>
2023-08-09 15:30:06 +0530

    Scheduler Added

--- src/main/java/com/example/AdFraudDetection/Configuration/DataInitialise.java
+++ src/main/java/com/example/AdFraudDetection/Configuration/DataInitialise.java
@@ -1,31 +1,42 @@
 package com.example.AdFraudDetection.Configuration;
 
 import com.example.AdFraudDetection.Class.IPData;
-import com.example.AdFraudDetection.Class.IPDetail;
 import com.example.AdFraudDetection.repository.IPDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import static java.lang.Math.abs;
 
 @Configuration
 @Component
-public class DataInitialise implements CommandLineRunner {
+public class DataInitialise {
+
 
     @Autowired
     private IPDataRepository ipDataRepo;
 
-    @Override
-    public void run(String... args)
+    private boolean dataExists=false;
+
+//    @Bean("DataInitialisation")
+//    @Scheduled(fixedRate = 10000)
+    public void addData()
     {
+        Random random = new Random();
+        int fraudInt = abs(random.nextInt()%10)+1;
+
+
         List ipDataList= new ArrayList();
+
         for(int i=0; i<256; i++)
         {
-            if(i%8==0)
+            if(i%fraudInt==0)
             {
                 ipDataList.add(new IPData("0.0.0."+String.valueOf(i), true, "India"));
             }
@@ -35,5 +46,8 @@ public class DataInitialise implements CommandLineRunner {
         }
 
         ipDataRepo.saveAll(ipDataList);
+        System.out.println("Data Initialised!");
+        System.out.println(fraudInt);
     }
+
 }
