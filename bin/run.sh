5f1edb33548bc91b9cd59d0c1c27e248eccc59c6
suryansh-singh_inmobi <suryansh.singh@inmobi.com>
2023-08-18 12:25:16 +0530

    logs

--- bin/run.sh
+++ bin/run.sh
@@ -3,9 +3,9 @@ mvn clean install -DskipTests
 docker rmi suryanshsingh/ad_fraud_detection:v3 -f
 docker build --tag=suryanshsingh/ad_fraud_detection:v3 ./bin/
 #
-#cd bin
-docker login --username suryanshsingh --password Devansh_123
-docker push suryanshsingh/ad_fraud_detection:v3
+cd bin
+#docker login --username suryanshsingh --password Devansh_123
+#docker push suryanshsingh/ad_fraud_detection:v3
 #
 #cd ..
 #minikube restart
@@ -13,4 +13,4 @@ docker push suryanshsingh/ad_fraud_detection:v3
 #kubectl rollout restart deployment
 #kubectl get pods
 
-#docker-compose up --build
+docker-compose up --build
